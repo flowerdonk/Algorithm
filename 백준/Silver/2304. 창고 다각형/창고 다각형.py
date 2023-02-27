10a26832b3db8837efb19fc087aedf766eaff262
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-02-27 16:25:07 +0900

    [Silver II] Title: 창고 다각형, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,47 +1,24 @@
 pillarN = int(input())
-store = [] * 1000
-input_pillar = []
-for i in range(pillarN):
-    input_pillar.append(list(map(int, input().split())))
+input_pillar = [list(map(int, input().split())) for _ in range(pillarN)]
+input_pillar.sort(key= lambda x : x[0])
+length = input_pillar[-1][0]
+mx_idx, mx_num = max(input_pillar, key = lambda x : x[1])
 
-max_L = 0
-max_H = 0
-max_H_idx = 0
-for i in range(pillarN):
-    if max_L <= input_pillar[i][0]:
-        max_L = input_pillar[i][0]
+store = [0] * (length + 1)
+for p in input_pillar:
+    store[p[0]] = p[1]
 
-for i in range(pillarN):
-    if max_H <= input_pillar[i][1]:
-        max_H = input_pillar[i][1]
-        max_H_idx = input_pillar[i][0]
+mx = 0
+area = mx_num
+for n in range(mx_idx):
+    if store[n] >= mx:
+        mx = store[n]
+    area += mx
 
-for i in range(1, max_L + 1):
-    count = 0
-    for r in range(pillarN):
-        if input_pillar[r][0] == i:
-            store.insert(i, input_pillar[r][1])
-            count += 1
-            break
-    
-    if count == 0:
-        store.insert(i, 0)
-
-area = 0
-big = 0
-for i in range(max_H_idx):
-    if i < max_H_idx:
-        if big < store[i]:
-            big = store[i]
-        area += big
-    elif i == max_H_idx:
-        area += max_H
-    
-big = 0
-for i in range(max_L - max_H_idx):
-    if big < store[max_L - 1 - i]:
-        big = store[max_L - 1 - i]
-    area += big
-    
+mx = 0
+for n in range(length, mx_idx, -1):
+    if store[n] >= mx:
+        mx = store[n]
+    area += mx
 
 print(area)
