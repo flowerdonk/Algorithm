21fdb721a356ed316310312d5963b45f510fd9d5
flowerdonk <dbwls5140@gmail.com>
2023-02-28 17:48:08 +0900

    ing

--- test.py
+++ test.py
@@ -1,32 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
 
-K = int(input())
-data = [list(map(int, input().split())) for _ in range(6)]
-l = []
-h = []
+L, H = map(int, input().split())
+N = int(input())
+# 1 : 북쪽(위), 2 : 남쪽(아래), 3 : 서쪽(왼), 4 : 동쪽(오)
+# 북, 남 -> 왼쪽부터의 거리 / 동, 서 -> 위쪽부터의 거리
+data = [list(map(int, input().split())) for _ in range(N)] # 방향, 거리
+x, y = map(int, input().split())
 
+face = {1 : 2, 2 : 1, 3 : 4, 4 : 1}
+
+distance = 0
 for d in data:
-    if d[0] == 1 or d[0] == 2:
-        l.append(d[1])
+    if d[0] == face[x]: # 마주보고 있을 때
+        n1 = H + x + d[1]
+        n2 = H + 2 * L - x - d[1]
+        n = n1 if n1 <= n2 else n2
+        distance += n
+    elif d[0] == x:
+        pass
     else:
-        h.append(d[1])
-
-l.sort()
-h.sort()
-
-length = l[-1]
-height = h[-1]
-
-for n in range(len(data)):
-    if data[n][1] == length or data[n][1] == height:
-        l_idx = n
-# 최종 길이가 5, 1 일때 5를 기준으로 하게 됨
-
-minus1 = data[(l_idx + 2) % 6][1]
-minus2 = data[(l_idx + 3) % 6][1]
-
-ans = length * height - minus1 * minus2
-print(ans * K)
-
-
+        pass
