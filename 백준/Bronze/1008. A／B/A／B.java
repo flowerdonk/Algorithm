f95508bded1c766dcda3f4a1320bebd1a9a0d0db
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-03-03 09:46:09 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 13040 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[])	{
+		Scanner sc = new Scanner(System.in);
+		double num1  = sc.nextInt();
+		double num2  = sc.nextInt();
+		System.out.print(num1 / num2);
+	}
+}
