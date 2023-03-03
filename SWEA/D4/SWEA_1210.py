33e54b3d2b640d1b4cc6a089596de3e23746e245
flowerdonk <dbwls5140@naver.com>
2023-03-03 21:10:05 +0900

    ladder1

--- SWEA/D4/SWEA_1210.py
+++ SWEA/D4/SWEA_1210.py
@@ -1,69 +1,30 @@
 import sys
 sys.stdin = open('input.txt')
 
-def Ladder(arr):
-    firstLine = []
-    for x in range(len(arr[0])):
-        if arr[0][x] == 1:
-            firstLine.append(x) # 시작점 좌표 담기
-    
-    keep_x = 0
-    keep_y = 0
+T = 10
+N = 100
+for tc in range(1, T + 1):
+    _ = input()
+    data = [list(input().split()) for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+    end_i, end_j = 0, 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == '2':
+                end_i, end_j = i, j
 
-    for start in firstLine: # 시작점 하나씩 반복
-        keep_x = start # 시작점 저장
-        y = 0
-        x = firstLine.index(start) # 시작점 x 인덱스
-
-        while x <= len(firstLine) - 1:
-            if y == 99: # 마지막 줄
-                if arr[y][firstLine[x]] == 2:
-                    return keep_x
-                else:
-                    break
-            #
-            # if x == 0: # 첫번째 열
-            #     if arr[y][firstLine[x] + 1] == 1:
-            #         keep_y = y # y좌표 저장
-            #         x += 1 # x 전 값으로
-            #         y += 1
-            #         continue # 다음 x좌표로
-            #     else:
-            #         y += 1
-            #
-            # elif x == len(firstLine) - 1: # 마지막 열
-            #     if arr[y][firstLine[x] - 1] == 1: # 왼쪽 길
-            #         keep_y = y # y좌표 저장
-            #         x -= 1
-            #         y += 1
-            #         continue
-            #     else:
-            #         y += 1
-
-            else:
-                if x != len(firstLine) - 1 and arr[y][firstLine[x] + 1] == 1: # 오른쪽 길
-                    keep_y = y # y좌표 저장
-                    x += 1 # x 전 값으로 
-                    y += 1
-                    continue # 다음 x좌표로
-
-                elif x != 0 and arr[y][firstLine[x] - 1] == 1: # 왼쪽 길
-                    keep_y = y # y좌표 저장
-                    x -= 1
-                    y += 1
-                    continue
-                
-                else:
-                    y += 1
-            
-    return -1
-
-
-result = []
-for _ in range(10):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(100)]
-    result.append(Ladder(arr))
-
-for i in range(10):
-    print(f'#{i + 1} {result[i]}')
+    direction = [[0, -1], [0, 1]] # 왼쪽, 오른쪽, 위
+    si, sj = end_i, end_j
+    while si != 0:
+        visited[si][sj] = 1
+        for d in direction:
+            ni = si + d[0]
+            nj = sj + d[1]
+            while 0 <= ni < N and 0 <= nj < N and data[ni][nj] == '1' and visited[ni][nj] == 0:
+                visited[si][sj] = 1
+                si = ni
+                sj = nj
+                ni += d[0]
+                nj += d[1]
+        si -= 1
+    print(f'#{tc} {sj}')
