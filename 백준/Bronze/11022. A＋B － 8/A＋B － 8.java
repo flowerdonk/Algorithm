124f7bfc1dfb328c004572d7f85b38a1967129b2
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-03-12 02:02:14 +0900

    [Bronze V] Title: A+B - 8, Time: 140 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.printf("Case #%d: %d + %d = %d\n", i, A, B, A + B);
+
+        }
+    }
+}
