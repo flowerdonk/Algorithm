b34bdf82adbb63e4a8f5649fdfc64dd1271fc1c3
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-03-14 14:04:57 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+        
+                      
+    }
+}
