89e52d91a79db930ebd53a45bfb9bff3e5487b55
flowerdonk <dbwls5140@naver.com>
2023-03-20 23:34:45 +0900

    파스칼 삼각형_김유진

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 from sys import stdin
 stdin = open('input.txt')
 '''
-끝나는 시간이 이른 순으로 정렬 -> 끝나는 시간이 같을 때를 고려해 시작 시간이 이른 순으로 한번 더 정렬([1, 2], [2, 2])
-시간 순으로 정렬했으니 데이터를 순회하며 그 다음 데이터가 이전 데이터의 끝나는 시간 이후인지를 확인 후 갱신
+
 '''
-N = int(stdin.readline()) # 회의 수
-data = [list(map(int, stdin.readline().split())) for _ in range(N)] # [시작, 끝]
-data.sort(key= lambda x: (x[1], x[0])) # 튜플로 여러 인자 전달 시 해당 순서대로 정렬
+R, C, W = map(int, stdin.readline().split())
+pascal = [[0] * n for n in range(1, R + W + 1)]
 
-end = data[0][1] # 가장 빨리 끝나는 것이 첫 번째 회의
-cnt = 1 # 시작 1 -> 첫 원소 포함
-for s, e in data[1:]: # 두 번째 부터
-    if s >= end:
-        end = e
-        cnt += 1
+pascal[0][0] = 1
+pascal[1][0], pascal[1][1] = 1, 1
 
-print(cnt)
+for n in range(1, R + W - 1):
+    pascal[n + 1][0] = 1
+    pascal[n + 1][n + 1] = 1
+    for m in range(n):
+        pascal[n + 1][m + 1] = pascal[n][m] + pascal[n][m + 1]
+
+ans = 0
+for i in range(W):
+    for j in range(i + 1):
+        ans += pascal[R + i - 1][C + j - 1]
+print(ans)
