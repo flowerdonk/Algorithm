8cf482277a31522ca96f920487f21c0969a4afa3
flowerdonk <dbwls5140@gmail.com>
2023-03-30 14:17:05 +0900

    30

--- SWEA/D4/1865.py
+++ SWEA/D4/1865.py
@@ -6,14 +6,18 @@ def task(idx, n):
         return
 
     for a in data[idx]:
-        n += a
-        task(idx + 1, n)
-        n -= a
+        if a != 0:
+            n *= a / 100
+            task(idx + 1, n)
+            n /= a / 100
+        else:
+            continue
 
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(N)] # idx 직원이 1~N번째 일을 성공할 확률 [, , ...,]
     mx = []
-    task(0, 0)
-    print(mx)
+    task(0, 1)
+    ans = max(mx) * 100
+    print('%.6f'%ans)
