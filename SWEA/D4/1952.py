57ad733ab7cc1553ea9c63c899a267b6c1de064d
flowerdonk <dbwls5140@gmail.com>
2023-03-31 10:27:25 +0900

    수영장

--- SWEA/D4/1952.py
+++ SWEA/D4/1952.py
@@ -1,23 +1,24 @@
 def dfs(n, sm):
     global ans
 
-    if ans == 0:
+    if ans <= sm:
         return
 
-    if n == N:
-        if sm >= B:
-            ans = min(ans, sm - B)
+    if n > 12:
+        ans =  min(ans, sm)
         return
 
-    dfs(n + 1, sm + data[n])
-    dfs(n + 1, sm)
+    dfs(n + 1, sm + D * data[n])
+    dfs(n + 1, sm + M)
+    dfs(n + 3, sm + T)
+    dfs(n + 12, sm + Y)
 
 T = int(input())
 for tc in range(1, T + 1):
-    N, B = map(int, input().split())
-    data = list(map(int, input().split()))
+    D, M, T, Y = map(int, input().split())
+    data = [0] + list(map(int, input().split()))
 
-    ans = N * 10000
-    dfs(0, 0)
+    ans = 365 * 3000
+    dfs(1, 0)
 
     print(f'#{tc} {ans}')
