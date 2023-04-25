77a7f54f01bae444ed37e597929908ce9abeb151
flowerdonk <dbwls5140@gmail.com>
2023-04-25 17:59:11 +0900

    0425

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
-N = int(input())
-board = [['*'] * N for _ in range(N)]
+import sys
+sys.stdin = open('input.txt')
 
-def blank(x, y, N):
-    l = N // 3
-    if board[x][y] == ' ':
-        return
-
-    for i in range(l, 2 * l):
-        for j in range(l, 2 * l):
-            board[x + i][y + j] = ' '
-
-    if l != 1:
-        for n in range(3):
-            for m in range(3):
-                blank(x + n * l,y + m * l, l)
-    return
-
-blank(0, 0, N)
-
-for i in range(N):
-    for j in range(N):
-        print(board[i][j], end='')
-    print()
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    parent = [[] for _ in range(N + 1)]
+    ancestor = [[] for _ in range(N + 1)]
+    # A가 B의 부모
+    for n in range(N - 1):
+        A, B = map(int, input().split())
+        parent[B].append(A)
+    start, end = map(int, input().split())
+    for node in range(N + 1):
+        stack = [parent[node]]
+        ancestor[node].append(parent[node])
+        while stack:
+            next_node = stack.pop()
+            ancestor[parent[node]].append(next_node)
+            stack.append(next_node)
+    print(ancestor) # 끝은 무조건 8(루트)
