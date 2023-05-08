adcabb78054ab032bdb341d4de06b19dbbba6227
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-05-08 17:54:17 +0900

    [Gold IV] Title: 타일 채우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,13 @@
+N = int(input())
+ans = 0
+dp = [0] * (N + 1)
+
+if not N % 2: # 짝수일 때
+    dp[2] = 3   
+    for n in range(4, N + 1, 2): # 4부터 짝수만
+        dp[n] = dp[2] * dp[n - 2] + 2
+        for m in range(4, n, 2): # 4칸을 차지하는 특수한 경우 2씩 추가
+            dp[n] += 2 * dp[n - m]
+
+ans = dp[N]
+print(ans)
