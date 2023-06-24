b5aa872676cba90029c85ab317d76073901547ad
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-06-24 23:44:18 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,8 @@
+-- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리
+-- 이름, 보호 시작일 조회
+-- 결과는 보호 시작일 순으로 조회
+
+SELECT I.NAME, I.DATETIME FROM ANIMAL_INS I
+LEFT JOIN ANIMAL_OUTS O ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE O.DATETIME IS NULL
+ORDER BY I.DATETIME LIMIT 3;
