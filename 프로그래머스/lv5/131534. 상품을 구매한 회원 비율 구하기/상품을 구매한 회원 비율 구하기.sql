56a9d33963ab4d941bd72dd2c44a60995588a865
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2023-06-25 00:46:13 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,4 +1,9 @@
--- 코드를 입력하세요
+-- 2021년에 가입한 전체 회원들 중 상품을 구매한 회원수와 상품을 구매한 회원 비율
+-- 비율 = 2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수
+-- 소수점 두번째자리에서 반올림
+-- 년, 월 별로 출력
+-- 년을 기준으로 오름차순, 년이 같다면 월을 기준으로 오름차순 정렬
+
 SELECT YEAR, MONTH, COUNT(*) AS PURCHASED_USERS, 
 ROUND((COUNT(*) / (SELECT COUNT(*) FROM USER_INFO WHERE YEAR(JOINED) = 2021)), 1) AS PURCHASED_RATIO
 FROM (
