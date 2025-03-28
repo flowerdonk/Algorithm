b0bf4782b5b1a164de30cd636a78446ec7b9fbfa
flowerdonk <122508480+flowerdonk@users.noreply.github.com>
2025-03-29 03:28:21 +0900

    [Gold V] Title: 숫자고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,26 +1,17 @@
-import sys
-
-N = int(input())
-nums = [0]
-result = set()
-for _ in range(N):
-    nums.append(int(input()))
-
-def dfs(idx, start):
-    visited[idx] = True
-    next_idx = nums[idx]
-    if visited[next_idx] and next_idx == start:
-        result.update({i for i, val in enumerate(visited) if val})
-        return
-    elif not visited[next_idx]:
-        dfs(next_idx, start)
-
-for i in range(1, N + 1):
-    visited = [False] * (N + 1)
-    dfs(i, i)
-
+def dfs(v,i):
+    visited[v]=True
+    w=data[v]
+    if not visited[w]:
+        dfs(w,i)
+    elif visited[w] and w==i:
+        result.append(w)
+n=int(input())
+data=[0]+[int(input()) for _ in range(n)]
+result=[]
+for i in range(1,n+1):
+    visited=[False]*(n+1)
+    dfs(i,i)
 print(len(result))
-result_list = list(result)
-result_list.sort()
-for i in result_list:
+result.sort()
+for i in result:
     print(i)
